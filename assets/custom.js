6342b1889f359c37fb80e6ad4a54bfd1fdd954bb
Renato Berrocal <rberrocal@belocode.com>
2022-10-16 22:01:24 -0500

    add measurements

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,770 @@
+/* START OF "Size Guide Data" */
+var data = {
+  bottom: {
+    regions: ['AU', 'US', 'EU', 'UK'],
+    names: {
+      AU: 'Australia / New Zealand',
+      US: 'USA / Latam',
+      EU: 'Europe / Japan',
+      UK: 'United Kingdom'
+    },
+    sizes: {
+      AU: [6, 8, 10, 12, 14, 16],
+      US: [2, 4, 6, 8, 10, 12],
+      EU: [34, 36, 38, 40, 42, 44],
+      UK: [6, 8, 10, 12, 14, 16]
+    },
+    results: {
+      AU6: {
+        size: 'XS',
+        measurements: [
+          'Waist: 65 - 69 cm',
+          'High hip: 85 - 88 cm'
+        ]
+      }, 
+      AU8: {
+        size: 'S',
+        measurements: [
+          'Waist: 70 - 73 cm',
+          'High hip: 89 - 91 cm'
+        ]
+      }, 
+      AU10: {
+        size: 'M',
+        measurements: [
+          'Waist: 74 - 78 cm',
+          'High hip: 92 - 94cm'
+        ]
+      }, 
+      AU12: {
+        size: 'L',
+        measurements: [
+          'Waist: 79 - 83 cm',
+          'High hip: 95 - 97cm'
+        ]
+      }, 
+      AU14: {
+        size: 'XL',
+        measurements: [
+          'Waist: 84 - 88 cm',
+          'High hip: 98 - 101 cm'
+        ]
+      }, 
+      AU16: {
+        size: 'XXL',
+        measurements: [
+          'Waist: 89 - 93 cm',
+          'High hip: 102 - 105 cm'
+        ]
+      },
+      US2: {
+        size: 'XS',
+        measurements: [
+          'Waist: 65 - 69 cm',
+          'High hip: 85 - 88 cm'
+        ]
+      }, 
+      US4: {
+        size: 'S',
+        measurements: [
+          'Waist: 70 - 73 cm',
+          'High hip: 89 - 91 cm'
+        ]
+      }, 
+      US6: {
+        size: 'M',
+        measurements: [
+          'Waist: 74 - 78 cm',
+          'High hip: 92 - 94cm'
+        ]
+      }, 
+      US8: {
+        size: 'L',
+        measurements: [
+          'Waist: 79 - 83 cm',
+          'High hip: 95 - 97cm'
+        ]
+      }, 
+      US10: {
+        size: 'XL',
+        measurements: [
+          'Waist: 84 - 88 cm',
+          'High hip: 98 - 101 cm'
+        ]
+      }, 
+      US12: {
+        size: 'XXL',
+        measurements: [
+          'Waist: 89 - 93 cm',
+          'High hip: 102 - 105 cm'
+        ]
+      },
+      EU34: {
+        size: 'XS',
+        measurements: [
+          'Waist: 65 - 69 cm',
+          'High hip: 85 - 88 cm'
+        ]
+      }, 
+      EU36: {
+        size: 'S',
+        measurements: [
+          'Waist: 70 - 73 cm',
+          'High hip: 89 - 91 cm'
+        ]
+      }, 
+      EU38: {
+        size: 'M',
+        measurements: [
+          'Waist: 74 - 78 cm',
+          'High hip: 92 - 94cm'
+        ]
+      }, 
+      EU40: {
+        size: 'L',
+        measurements: [
+          'Waist: 79 - 83 cm',
+          'High hip: 95 - 97cm'
+        ]
+      }, 
+      EU42: {
+        size: 'XL',
+        measurements: [
+          'Waist: 84 - 88 cm',
+          'High hip: 98 - 101 cm'
+        ]
+      }, 
+      EU4: {
+        size: 'XXL',
+        measurements: [
+          'Waist: 89 - 93 cm',
+          'High hip: 102 - 105 cm'
+        ]
+      },
+      UK6: {
+        size: 'XS',
+        measurements: [
+          'Waist: 65 - 69 cm',
+          'High hip: 85 - 88 cm'
+        ]
+      }, 
+      UK8: {
+        size: 'S',
+        measurements: [
+          'Waist: 70 - 73 cm',
+          'High hip: 89 - 91 cm'
+        ]
+      }, 
+      UK10: {
+        size: 'M',
+        measurements: [
+          'Waist: 74 - 78 cm',
+          'High hip: 92 - 94cm'
+        ]
+      }, 
+      UK12: {
+        size: 'L',
+        measurements: [
+          'Waist: 79 - 83 cm',
+          'High hip: 95 - 97cm'
+        ]
+      }, 
+      UK14: {
+        size: 'XL',
+        measurements: [
+          'Waist: 84 - 88 cm',
+          'High hip: 98 - 101 cm'
+        ]
+      }, 
+      UK16: {
+        size: 'XXL',
+        measurements: [
+          'Waist: 89 - 93 cm',
+          'High hip: 102 - 105 cm'
+        ]
+      }
+    }
+  },
+  top: {
+    regions: ['AU', 'US', 'EU', 'UK', 'FR', 'CZ'],
+    names: {
+      AU: 'Australia / New Zealand',
+      US: 'USA / Latam',
+      EU: 'Europe / Japan',
+      UK: 'United Kingdom',
+      FR: 'France / Spain / Belgium',
+      CZ: 'Czech Republic / Italy'
+    },
+    sizes: {
+      AU: [8, 10, 12, 14, 16],
+      US: [30, 32, 34, 36, 38],
+      EU: [65, 70, 75, 80, 85],
+      UK: [30, 32, 34, 36, 38],
+      FR: [80, 85, 90, 95, 100],
+      CZ: [0, 1, 2, 3, 4]
+    },
+    cups: {
+      AU8: ['AA', 'A'],
+      AU10: ['AA', 'A', 'B', 'C', 'D', 'DD'],
+      AU12: ['A', 'B', 'C', 'D'],
+      AU14: ['A', 'B', 'C', 'D'],
+      AU16: ['AA', 'A', 'B', 'C'],
+      US30: ['AA', 'A'],
+      US32: ['AA', 'A', 'B', 'C', 'D', 'DD', 'DDD/E'],
+      US34: ['A', 'B', 'C', 'D', 'DD'],
+      US36: ['A', 'B', 'C', 'D', 'DD'],
+      US38: ['A', 'B', 'C', 'D'],
+      EU65: ['A', 'B'],
+      EU70: ['A', 'B', 'C', 'D', 'E', 'F'],
+      EU75: ['A', 'B', 'C', 'D', 'E'],
+      EU80: ['A', 'B', 'C', 'D', 'E'],
+      EU85: ['A', 'B', 'C', 'D'],
+      UK30: ['A', 'B'],
+      UK32: ['A', 'B', 'C', 'D', 'DD', 'E'],
+      UK34: ['A', 'B', 'C', 'D', 'DD'],
+      UK36: ['A', 'B', 'C', 'D', 'DD'],
+      UK38: ['A', 'B', 'C', 'D'],
+      FR80: ['A', 'B'],
+      FR85: ['A', 'B', 'C', 'D', 'E', 'F'],
+      FR90: ['A', 'B', 'C', 'D', 'E'],
+      FR95: ['A', 'B', 'C', 'D', 'E'],
+      FR100: ['A', 'B', 'C', 'D'],
+      CZ0: ['A', 'B'],
+      CZ1: ['A', 'B', 'C', 'D', 'E', 'F'],
+      CZ2: ['A', 'B', 'C', 'D', 'E'],
+      CZ3: ['A', 'B', 'C', 'D', 'E'],
+      CZ4: ['A', 'B', 'C', 'D']
+    },
+    results: {
+      AU8AA: { 
+        size: 'XS',
+        measurements: []
+      },
+      AU8A: { 
+        size: 'XS',
+        measurements: []
+      },
+      AU10AA: { 
+        size: 'XS',
+        measurements: []
+      },
+      AU10A: { 
+        size: 'XS/S',
+        measurements: []
+      },
+      AU10B: { 
+        size: 'M',
+        measurements: []
+      },
+      AU10C: { 
+        size: 'M/L',
+        measurements: []
+      },
+      AU10D: { 
+        size: 'L',
+        measurements: []
+      },
+      AU10DD: { 
+        size: 'XL',
+        measurements: []
+      },
+      AU12A: { 
+        size: 'XS/S',
+        measurements: []
+      },
+      AU12B: { 
+        size: 'M',
+        measurements: []
+      },
+      AU12C: { 
+        size: 'L',
+        measurements: []
+      },
+      AU12D: { 
+        size: 'XL',
+        measurements: []
+      },
+      AU14A: { 
+        size: 'S/M',
+        measurements: []
+      },
+      AU14B: { 
+        size: 'L',
+        measurements: []
+      },
+      AU14C: { 
+        size: 'XL',
+        measurements: []
+      },
+      AU14D: { 
+        size: 'XXL',
+        measurements: []
+      },
+      AU16AA: { 
+        size: 'M',
+        measurements: []
+      },
+      AU16A: { 
+        size: 'L',
+        measurements: []
+      },
+      AU16B: { 
+        size: 'XL',
+        measurements: []
+      },
+      AU16C: { 
+        size: 'XXL',
+        measurements: []
+      },
+      US30AA: { 
+        size: 'XS',
+        measurements: []
+      },
+      US30A: { 
+        size: 'XS',
+        measurements: []
+      },
+      US32AA: { 
+        size: 'XS',
+        measurements: []
+      },
+      US32A: { 
+        size: 'XS',
+        measurements: []
+      },
+      US32B: { 
+        size: 'S',
+        measurements: []
+      },
+      US32C: { 
+        size: 'M',
+        measurements: []
+      },
+      US32D: { 
+        size: 'M/L',
+        measurements: []
+      },
+      US32DD: { 
+        size: 'L',
+        measurements: []
+      },
+      US32DDDE: { 
+        size: 'XL',
+        measurements: []
+      },
+      US34A: { 
+        size: 'XS',
+        measurements: []
+      },
+      US34B: { 
+        size: 'S',
+        measurements: []
+      },
+      US34C: { 
+        size: 'M',
+        measurements: []
+      },
+      US34D: { 
+        size: 'L',
+        measurements: []
+      },
+      US34DD: { 
+        size: 'XL',
+        measurements: []
+      },
+      US36A: { 
+        size: 'S',
+        measurements: []
+      },
+      US36B: { 
+        size: 'M',
+        measurements: []
+      },
+      US36C: { 
+        size: 'L',
+        measurements: []
+      },
+      US36D: { 
+        size: 'XL',
+        measurements: []
+      },
+      US36DD: { 
+        size: 'XXL',
+        measurements: []
+      },
+      US38A: { 
+        size: 'M',
+        measurements: []
+      },
+      US38B: { 
+        size: 'L',
+        measurements: []
+      },
+      US38C: { 
+        size: 'XL',
+        measurements: []
+      },
+      US38D: { 
+        size: 'XXL',
+        measurements: []
+      },
+      UK30A: { 
+        size: 'XS',
+        measurements: []
+      },
+      UK30B: { 
+        size: 'XS',
+        measurements: []
+      },
+      UK32A: { 
+        size: 'XS',
+        measurements: []
+      },
+      UK32B: { 
+        size: 'S',
+        measurements: []
+      },
+      UK32C: { 
+        size: 'M',
+        measurements: []
+      },
+      UK32D: { 
+        size: 'M/L',
+        measurements: []
+      },
+      UK32DD: { 
+        size: 'L',
+        measurements: []
+      },
+      UK32E: { 
+        size: 'XL',
+        measurements: []
+      },
+      UK34A: { 
+        size: 'XS',
+        measurements: []
+      },
+      UK34B: { 
+        size: 'S',
+        measurements: []
+      },
+      UK34C: { 
+        size: 'M',
+        measurements: []
+      },
+      UK34D: { 
+        size: 'L',
+        measurements: []
+      },
+      UK34DD: { 
+        size: 'XL',
+        measurements: []
+      },
+      UK36A: { 
+        size: 'S',
+        measurements: []
+      },
+      UK36B: { 
+        size: 'M',
+        measurements: []
+      },
+      UK36C: { 
+        size: 'L',
+        measurements: []
+      },
+      UK36D: { 
+        size: 'XL',
+        measurements: []
+      },
+      UK36DD: { 
+        size: 'XXL',
+        measurements: []
+      },
+      UK38A: { 
+        size: 'M',
+        measurements: []
+      },
+      UK38B: { 
+        size: 'L',
+        measurements: []
+      },
+      UK38C: { 
+        size: 'XL',
+        measurements: []
+      },
+      UK38D: { 
+        size: 'XXL',
+        measurements: []
+      },
+      EU65A: { 
+        size: 'XS',
+        measurements: []
+      },
+      EU65B: { 
+        size: 'XS',
+        measurements: []
+      },
+      EU70A: { 
+        size: 'XS',
+        measurements: []
+      },
+      EU70B: { 
+        size: 'S',
+        measurements: []
+      },
+      EU70C: { 
+        size: 'M',
+        measurements: []
+      },
+      EU70D: { 
+        size: 'M/L',
+        measurements: []
+      },
+      EU70E: { 
+        size: 'L',
+        measurements: []
+      },
+      EU70F: { 
+        size: 'XL',
+        measurements: []
+      },
+      EU75A: { 
+        size: 'XS',
+        measurements: []
+      },
+      EU75B: { 
+        size: 'S',
+        measurements: []
+      },
+      EU75C: { 
+        size: 'M',
+        measurements: []
+      },
+      EU75D: { 
+        size: 'L',
+        measurements: []
+      },
+      EU75E: { 
+        size: 'XL',
+        measurements: []
+      },
+      EU80A: { 
+        size: 'S',
+        measurements: []
+      },
+      EU80B: { 
+        size: 'M',
+        measurements: []
+      },
+      EU80C: { 
+        size: 'L',
+        measurements: []
+      },
+      EU80D: { 
+        size: 'XL',
+        measurements: []
+      },
+      EU80E: { 
+        size: 'XXL',
+        measurements: []
+      },
+      EU85A: { 
+        size: 'M',
+        measurements: []
+      },
+      EU85B: { 
+        size: 'L',
+        measurements: []
+      },
+      EU85C: { 
+        size: 'XL',
+        measurements: []
+      },
+      EU85D: { 
+        size: 'XXL',
+        measurements: []
+      },
+      FR80A: { 
+        size: 'XS',
+        measurements: []
+      },
+      FR80B: { 
+        size: 'XS',
+        measurements: []
+      },
+      FR85A: { 
+        size: 'XS',
+        measurements: []
+      },
+      FR85B: { 
+        size: 'S',
+        measurements: []
+      },
+      FR85C: { 
+        size: 'M',
+        measurements: []
+      },
+      FR85D: { 
+        size: 'M/L',
+        measurements: []
+      },
+      FR85E: { 
+        size: 'L',
+        measurements: []
+      },
+      FR85F: { 
+        size: 'XL',
+        measurements: []
+      },
+      FR90A: { 
+        size: 'XS',
+        measurements: []
+      },
+      FR90B: { 
+        size: 'S',
+        measurements: []
+      },
+      FR90C: { 
+        size: 'M',
+        measurements: []
+      },
+      FR90D: { 
+        size: 'L',
+        measurements: []
+      },
+      FR90E: { 
+        size: 'XL',
+        measurements: []
+      },
+      FR95A: { 
+        size: 'S',
+        measurements: []
+      },
+      FR95B: { 
+        size: 'M',
+        measurements: []
+      },
+      FR95C: { 
+        size: 'L',
+        measurements: []
+      },
+      FR95D: { 
+        size: 'XL',
+        measurements: []
+      },
+      FR95E: { 
+        size: 'XXL',
+        measurements: []
+      },
+      FR100A: { 
+        size: 'M',
+        measurements: []
+      },
+      FR100B: { 
+        size: 'L',
+        measurements: []
+      },
+      FR100C: { 
+        size: 'XL',
+        measurements: []
+      },
+      FR100D: { 
+        size: 'XXL',
+        measurements: []
+      },
+      CZ0A: { 
+        size: 'XS',
+        measurements: []
+      },
+      CZ0B: { 
+        size: 'XS',
+        measurements: []
+      },
+      CZ1A: { 
+        size: 'XS',
+        measurements: []
+      },
+      CZ1B: { 
+        size: 'S',
+        measurements: []
+      },
+      CZ1C: { 
+        size: 'M',
+        measurements: []
+      },
+      CZ1D: { 
+        size: 'M/L',
+        measurements: []
+      },
+      CZ1E: { 
+        size: 'L',
+        measurements: []
+      },
+      CZ1F: { 
+        size: 'XL',
+        measurements: []
+      },
+      CZ2A: { 
+        size: 'XS',
+        measurements: []
+      },
+      CZ2B: { 
+        size: 'S',
+        measurements: []
+      },
+      CZ2C: { 
+        size: 'M',
+        measurements: []
+      },
+      CZ2D: { 
+        size: 'L',
+        measurements: []
+      },
+      CZ2E: { 
+        size: 'XL',
+        measurements: []
+      },
+      CZ3A: { 
+        size: 'S',
+        measurements: []
+      },
+      CZ3B: { 
+        size: 'M',
+        measurements: []
+      },
+      CZ3C: { 
+        size: 'L',
+        measurements: []
+      },
+      CZ3D: { 
+        size: 'XL',
+        measurements: []
+      },
+      CZ3E: { 
+        size: 'XXL',
+        measurements: []
+      },
+      CZ4A: { 
+        size: 'M',
+        measurements: []
+      },
+      CZ4B: { 
+        size: 'L',
+        measurements: []
+      },
+      CZ4C: { 
+        size: 'XL',
+        measurements: []
+      },
+      CZ4D: { 
+        size: 'XXL',
+        measurements: []
+      }
+    }
+  }
+};
+/* END OF "Size GuideData " */
+
 (function(){
   const originalAddClassMethod = jQuery.fn.addClass;
   const originalRemoveClassMethod = jQuery.fn.removeClass;
